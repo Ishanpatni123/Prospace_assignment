a362141d1e8b94640988ac9ee07b51066051d6b8
Ishan Patni <ishanpatni123@gmail.com>
2024-03-25 17:48:47 +0530

    minor changes

--- users/views.py
+++ users/views.py
@@ -2,12 +2,11 @@ from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
-# Create your views here.
 
-from users.forms import profileUpdateForm, userUpdateForm, RequirementsForm
+
+from users.forms import RequirementsForm
 from users.models import Profile 
-from users.models import  Requirements
-from django.contrib import messages
+
 
 
 @login_required(login_url='login')
@@ -32,9 +31,6 @@ def SignupPage(request):
             profile = Profile.objects.create(user = my_user)
             return redirect('login')
         
-
-
-
     return render (request,'signup.html')
 
 def LoginPage(request):
@@ -55,10 +51,7 @@ def LogoutPage(request):
     return redirect('login')
 
 # decorator
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from .models import Profile
+
 
 @login_required(login_url='login')
 def profile(request):
@@ -67,7 +60,6 @@ def profile(request):
         profile = Profile.objects.get(user=request.user)
         profile.bio = new_bio
         profile.save()
-        # messages.success(request, 'Your bio was successfully updated!')
         return redirect('Profile')
     else:
         profile = Profile.objects.get(user=request.user)
@@ -85,17 +77,13 @@ def save_requirements(request):
     if request.method == 'POST':
         form = RequirementsForm(request.POST)
         if form.is_valid():
-            # If the form is valid, save the data to the database
+            
             requirements = form.save(commit=False)
-            # Populate the user field with the currently logged-in user
             requirements.user = request.user
-            # Save the object to the database
             requirements.save()
-            # Redirect to the home page or wherever appropriate
             return redirect('home')
     else:
         form = RequirementsForm()
     
-    # Render the form template with the form
     return render(request, 'requirements_form.html', {'form': form})
 
